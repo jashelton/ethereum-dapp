c152a5f7755e8791b9adece028ec75d1513f65ff
jashelton <justin.shelton@yahoo.com>
2017-03-25 12:28:27 -0400

    adding getBalance and transfer methods

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,14 +1,33 @@
 pragma solidity ^0.4.4;
 
 contract HelloWorld {
-  uint public balance;
+
+  address public owner;
+  mapping ( address => uint) balances;
 
   function HelloWorld() {
-    balance = 1000;
+    //Takes on the value of the address for the person who calls the contract
+    owner = msg.sender;
+    balances[owner] = 1000;
   }
 
-  function deposit(uint _value) returns(uint _newValue) {
-    balance += _value;
-    return balance;
+  event voteCast(address _to, uint _value, address owner);
+
+  function transfer(address _to, uint _value) returns (bool success) {
+    voteCast(_to, _value, owner);
+    
+    if (balances[msg.sender] < _value) {
+      return false;
+    }
+
+    balances[msg.sender] -= _value;
+    balances[_to] += _value;
+    return true;
   }
+
+  //Constant says that this is a function that doesn't change the state of the blockchain.
+  function getBalance(address _user) constant returns (uint) {
+    return balances[_user];
+  }
+
 }
